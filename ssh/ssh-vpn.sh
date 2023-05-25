5f1380812caf94053d2507a23c19ce2683430f83
mofukuy <122971182+mofukuy@users.noreply.github.com>
2023-05-25 21:54:19 +0700

    Update ssh-vpn.sh

--- ssh/ssh-vpn.sh
+++ ssh/ssh-vpn.sh
@@ -17,7 +17,7 @@ locality=none
 organization=none
 organizationalunit=none
 commonname=none
-email=adamspx17@gmail.com
+email=mofukuy@gmail.com
 
 # simple password minimal
 curl -sS https://raw.githubusercontent.com/artanodrop/v4/main/ssh/password | openssl aes-256-cbc -d -a -pass pass:scvps07gg -pbkdf2 > /etc/pam.d/common-password
@@ -271,16 +271,16 @@ netfilter-persistent reload
 # download script
 cd /usr/bin
 # menu
-wget -O menu "https://raw.githubusercontent.com/fikripps/v4/main/menu/menu.sh"
-wget -O menu-vmess "https://raw.githubusercontent.com/fikripps/v4/main/menu/menu-vmess.sh"
-wget -O menu-vless "https://raw.githubusercontent.com/fikripps/v4/main/menu/menu-vless.sh"
-wget -O running "https://raw.githubusercontent.com/fikripps/v4/main/menu/running.sh"
-wget -O clearcache "https://raw.githubusercontent.com/fikripps/v4/main/menu/clearcache.sh"
-wget -O menu-trgo "https://raw.githubusercontent.com/fikripps/v4/main/menu/menu-trgo.sh"
-wget -O menu-trojan "https://raw.githubusercontent.com/fikripps/v4/main/menu/menu-trojan.sh"
+wget -O menu "https://raw.githubusercontent.com/mofukuy/v4/main/menu/menu.sh"
+wget -O menu-vmess "https://raw.githubusercontent.com/mofukuy/v4/main/menu/menu-vmess.sh"
+wget -O menu-vless "https://raw.githubusercontent.com/mofukuy/v4/main/menu/menu-vless.sh"
+wget -O running "https://raw.githubusercontent.com/mofukuy/v4/main/menu/running.sh"
+wget -O clearcache "https://raw.githubusercontent.com/mofukuy/v4/main/menu/clearcache.sh"
+wget -O menu-trgo "https://raw.githubusercontent.com/mofukuy/v4/main/menu/menu-trgo.sh"
+wget -O menu-trojan "https://raw.githubusercontent.com/mofukuy/v4/main/menu/menu-trojan.sh"
 
 # menu ssh ovpn
-wget -O menu-ssh "https://raw.githubusercontent.com/fikripps/v4/main/menu/menu-ssh.sh"
+wget -O menu-ssh "https://raw.githubusercontent.com/mofukuy/v4/main/menu/menu-ssh.sh"
 wget -O usernew "https://raw.githubusercontent.com/artanodrop/v4/main/ssh/usernew.sh"
 wget -O trial "https://raw.githubusercontent.com/artanodrop/v4/main/ssh/trial.sh"
 wget -O renew "https://raw.githubusercontent.com/artanodrop/v4/main/ssh/renew.sh"
@@ -293,17 +293,17 @@ wget -O ceklim "https://raw.githubusercontent.com/artanodrop/v4/main/ssh/ceklim.
 wget -O tendang "https://raw.githubusercontent.com/artanodrop/v4/main/ssh/tendang.sh"
 
 # menu system
-wget -O menu-set "https://raw.githubusercontent.com/fikripps/v4/main/menu/menu-set.sh"
-wget -O menu-domain "https://raw.githubusercontent.com/fikripps/v4/main/menu/menu-domain.sh"
+wget -O menu-set "https://raw.githubusercontent.com/mofukuy/v4/main/menu/menu-set.sh"
+wget -O menu-domain "https://raw.githubusercontent.com/mofukuy/v4/main/menu/menu-domain.sh"
 wget -O add-host "https://raw.githubusercontent.com/artanodrop/v4/main/ssh/add-host.sh"
 wget -O port-change "https://raw.githubusercontent.com/artanodrop/v4/main/port/port-change.sh"
 wget -O certv2ray "https://raw.githubusercontent.com/artanodrop/v4/main/xray/certv2ray.sh"
-wget -O menu-webmin "https://raw.githubusercontent.com/fikripps/v4/main/menu/menu-webmin.sh"
+wget -O menu-webmin "https://raw.githubusercontent.com/mofukuy/v4/main/menu/menu-webmin.sh"
 wget -O speedtest "https://raw.githubusercontent.com/artanodrop/v4/main/ssh/speedtest_cli.py"
-wget -O about "https://raw.githubusercontent.com/fikripps/v4/main/menu/about.sh"
-wget -O auto-reboot "https://raw.githubusercontent.com/fikripps/v4/main/menu/auto-reboot.sh"
-wget -O restart "https://raw.githubusercontent.com/fikripps/v4/main/menu/restart.sh"
-wget -O bw "https://raw.githubusercontent.com/fikripps/v4/main/menu/bw.sh"
+wget -O about "https://raw.githubusercontent.com/mofukuy/v4/main/menu/about.sh"
+wget -O auto-reboot "https://raw.githubusercontent.com/mofukuy/v4/main/menu/auto-reboot.sh"
+wget -O restart "https://raw.githubusercontent.com/mofukuy/v4/main/menu/restart.sh"
+wget -O bw "https://raw.githubusercontent.com/mofukuy/v4/main/menu/bw.sh"
 
 # change port
 wget -O port-ssl "https://raw.githubusercontent.com/artanodrop/v4/main/port/port-ssl.sh"
@@ -315,8 +315,8 @@ wget -O acs-set "https://raw.githubusercontent.com/artanodrop/v4/main/acs-set.sh
 
 wget -O sshws "https://raw.githubusercontent.com/artanodrop/v4/main/ssh/sshws.sh"
 wget -O status "https://raw.githubusercontent.com/artanodrop/v4/main/status.sh"
-wget -O run "https://raw.githubusercontent.com/fikripps/v4/main/run.sh"
-wget -O jam "https://raw.githubusercontent.com/fikripps/v4/main/jam.sh"
+wget -O run "https://raw.githubusercontent.com/mofukuy/v4/main/run.sh"
+wget -O jam "https://raw.githubusercontent.com/mofukuy/v4/main/jam.sh"
 
 
 chmod +x menu
